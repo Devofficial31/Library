c6afdebbed40f0425b87747ebdc1581e83b6a059
Debabrata Mishra <dbmishra00@gmail.com>
2023-08-10 12:58:51 +0530

    first read the txt fie after that you run .java

---
+++ DB.java
@@ -0,0 +1,20 @@
+package Library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","","");
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return con;
+	}
+
+}
+
